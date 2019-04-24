4214c9da15fbd04313d01ec593265a3a88ed539b
John rowley <jrowley@student.ibat>
2019-04-24 20:11:50 +0100

    adding list

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
 
+/*
 echo "Hello World. This is Version 2";
 echo "<p>Password is " . getenv('DB_PASSWORD');
+*/
+
+class DBike {
+    
+    public $number;
+  
+    public $name;
+  
+
+}
+
+$listOfBikes = [];
+
+for($i=0; $i < 10; i++) {
+
+    $bike = new DBike;
+    $bike->number=$i;
+    $bike->name="Name $i";
+
+    $listOfBikes[]=$bike;
+
+}
+
+print_r($listOfBikes);
